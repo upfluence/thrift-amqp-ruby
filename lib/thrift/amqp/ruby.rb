ae86c7b7ba37c04f8542459be859273e1f353d27
Alexis Montagne <alexis.montagne@gmail.com>
2015-02-27 15:57:13 +0100

    Reboot the repo as a gem

---
+++ lib/thrift/amqp/ruby.rb
@@ -0,0 +1,10 @@
+require "thrift/amqp/ruby/version"
+require "thrift/amqp/amqp_rpc_client"
+require "thrift/amqp/amqp_rpc_service"
+
+module Thrift
+  module Amqp
+    module Ruby
+    end
+  end
+end
---
+++ lib/thrift/amqp/ruby/version.rb
@@ -0,0 +1,7 @@
+module Thrift
+  module Amqp
+    module Ruby
+      VERSION = "0.0.1"
+    end
+  end
+end
